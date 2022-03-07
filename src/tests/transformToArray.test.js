e2bccb410790140260678382a1dfd2f26a7bb98b
Nicolás Mosi <57416731+krab-e@users.noreply.github.com>
2022-03-07 02:32:29 -0300

    Delete transformToArray.test.js

--- src/tests/transformToArray.test.js
+++
@@ -1,8 +0,0 @@
-import { transformToArray } from '..';
-import { resolve } from 'path';
-
-describe('Given a .txt file', () => {
-  it('should return an array composed of the file lines', () => {
-    const stringArr = transformToArray(resolve('.') + '/discography.txt');
-  });
-});
